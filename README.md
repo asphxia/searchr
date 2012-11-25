971cc60b0a782cda9483edba8bbd97f3e859a9fd
Dario <carlos.cavuotti@gmail.com>
2012-11-25 08:54:49 -0300

    Added deploy script (phing)

---
+++ README.md
@@ -0,0 +1,15 @@
+HashSearchr
+=================================
+HashSearchr is a simple tool that uses [Md5Decrypter] library to search for MD5 hash using a series of online web-based services, such as Google, Md5Decrypter.co.uk and so on.
+
+Contributing
+============
+There are a few rules to contributing to this project:
+*   No tab characters are allowed (all indents should be 4 spaces). You can configure this in your editor.
+*   Please test your code and make sure it works properly before submitting a pull request.
+
+Requirements
+============
+*   PHP >= 5.3
+*   cURL extension
+*   file_url_open enabled
---
+++ web/index.php
@@ -0,0 +1,29 @@
+<?php
+require_once __DIR__.'/../vendor/autoload.php';
+use Symfony\Component\HttpFoundation\Request;
+
+$app = new Silex\Application();
+
+$app['debug'] = true;
+
+$app->get('/', function() {
+    return '';
+});
+
+$app->get('/decrypter/hash/', function(Silex\Application $app, Request $request) {
+    $decryptors = array('Web\Noisette','Web\Myaddr','Web\Decrypt','Web\Md5decrypter','Web\Google', 'Web\Gromweb');
+
+    $result = array();
+    $result['hash'] = $request->get('hash');
+    foreach($decryptors as $decrytor)
+    {
+        if (NULL !== ($plain = Asphxia\Md5Decryptor\Md5Decryptor::plain($hash, $decrytor))) {
+            $result['plain'] = $plain;
+            $result['decryptor'] = $decryptor;
+            break;
+        }
+    }
+    return $app->json($result);
+});
+
+$app->run();
