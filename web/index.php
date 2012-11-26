69b620cdd6661ca542dc61dd89ce627ee3a81059
Dario <carlos.cavuotti@gmail.com>
2012-11-25 22:13:47 -0300

    Feature: hash parameter now allows for multiple hashes, separated by "|" or the separator given in the separator param.

--- web/index.php
+++ web/index.php
@@ -10,13 +10,25 @@ $app->get('/', function(Silex\Application $app, Request $request) {
     $decryptors = array('Web\Noisette','Web\Myaddr','Web\Decrypt','Web\Md5decrypter','Web\Google', 'Web\Gromweb');
 
     $result = array();
-    $result['hash'] = $request->get('hash');
-    foreach($decryptors as $decryptor)
+    $splitter = $request->get('separator') ? $request->get('separator') : '|';
+    $hashes = $request->get('hash');
+    
+    if (strpos($hashes, $splitter) !== false) {
+        $hashes = explode($splitter, $hashes);
+    }else{
+        $hashes = array($hashes);
+    }
+
+    foreach ($hashes as $hash)
     {
-        if (NULL !== ($plain = Asphxia\Md5Decryptor\Md5Decryptor::plain($result['hash'], $decryptor))) {
-            $result['plain'] = $plain;
-            $result['decryptor'] = $decryptor;
-            break;
+        $result[]['hash'] = $hash;
+        foreach($decryptors as $decryptor)
+        {
+            if (NULL !== ($plain = Asphxia\Md5Decryptor\Md5Decryptor::plain($hash, $decryptor))) {
+                $result[]['plain'] = $plain;
+                $result[]['decryptor'] = $decryptor;
+                break;
+            }
         }
     }
     return $app->json($result);
